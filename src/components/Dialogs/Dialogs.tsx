591f839550fd6be56c4ae42e3a681e6767bea6d9
gargandiz <gargandzolla10@gmail.com>
2022-05-08 08:25:53 +0300

    lesson_21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import classes from "./Dialogs.module.css"
 
 const Dialogs = () => {
-    return <div>Dialogs</div>
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItem}>
+                <div className={classes.dialog + " " + classes.active}>
+                    Dimon
+                </div>
+            </div>
+            <div className={classes.massages}>
+                <div className={classes.massage}>Hi</div>
+                <div className={classes.massage}>How do you do?</div>
+            </div>
+        </div>
+    )
 }
 
 export default Dialogs;
