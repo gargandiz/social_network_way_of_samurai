caf9bbf88e0f723b4449f06cead0999f4342a6b0
gargandiz <gargandzolla10@gmail.com>
2022-05-11 19:09:24 +0300

    lesson_24

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -11,6 +11,22 @@ type MassagePropsType = {
     massage: string
 }
 
+const DialogsData = [
+    {id: 1, name: "Zheka"},
+    {id: 2, name: "Dimon"},
+    {id: 3, name: "Yana"},
+    {id: 4, name: "Albina"},
+    {id: 5, name: "Vovan"}
+]
+
+const MassageData = [
+    {id: 1, massage: "Hi",},
+    {id: 2, massage: "How do you do?"},
+    {id: 3, massage: "I'm glad to see you"},
+    {id: 4, massage: "I have a sore throat"},
+    {id: 5, massage: "Let's have lunch"}
+]
+
 const DialogsItem = (props: DialogsItemPropsType) => {
   return (
       <div className={classes.dialog + " " + classes.active}>
@@ -29,16 +45,18 @@ const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItem}>
-                <DialogsItem name={"Zheka"} id={1} />
-                <DialogsItem name={"Dimon"} id={2} />
-                <DialogsItem name={"Yana"} id={3} />
-                <DialogsItem name={"Albina"} id={4} />
-                <DialogsItem name={"Vovan"} id={5} />
+                <DialogsItem name={DialogsData[0].name} id={DialogsData[0].id} />
+                <DialogsItem name={DialogsData[1].name} id={DialogsData[1].id} />
+                <DialogsItem name={DialogsData[2].name} id={DialogsData[1].id} />
+                <DialogsItem name={DialogsData[3].name} id={DialogsData[1].id} />
+                <DialogsItem name={DialogsData[4].name} id={DialogsData[1].id} />
             </div>
             <div className={classes.massages}>
-                <Massage massage={"Hi"} />
-                <Massage massage={"How do you do?"} />
-                <Massage massage={"I'm glad to see you"} />
+                <Massage massage={MassageData[0].massage} />
+                <Massage massage={MassageData[1].massage} />
+                <Massage massage={MassageData[2].massage} />
+                <Massage massage={MassageData[3].massage} />
+                <Massage massage={MassageData[4].massage} />
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,13 @@ import Post from "./Post/Post";
 import classes from "./MyPosts.module.css";
 
 
+const PostData = [
+    {id: 1,message: "Hello", likesCount: 15},
+    {id: 2,message: "In the middle", likesCount: 2},
+    {id: 3,message: "At the top", likesCount: 24},
+    {id: 4,message: "On the right", likesCount: 42}
+]
+
 const MyPosts = () => {
     return (
         <div className={classes.postsBlock}>
@@ -18,8 +25,10 @@ const MyPosts = () => {
 
             </div>
             <div className={classes.postsBlock}>
-                <Post message={"Hello"} likesCount={15} />
-                <Post message={"What is this"} likesCount={25} />
+                <Post message={PostData[0].message} likesCount={PostData[0].likesCount} />
+                <Post message={PostData[1].message} likesCount={PostData[1].likesCount} />
+                <Post message={PostData[2].message} likesCount={PostData[2].likesCount} />
+                <Post message={PostData[3].message} likesCount={PostData[3].likesCount} />
             </div>
         </div>
     )
